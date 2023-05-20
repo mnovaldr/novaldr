3452f8300b06b42b53d8c18c9ffbed342148dbb3
mnovaldr <noval.compani@gmail.com>
2023-05-20 11:15:09 +0700

    Update dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,10 +2,15 @@
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.add('flex');
+        toTop.classList.remove('hidden');
     }else {
+        toTop.classList.add('hidden');
+        toTop.classList.remove('flex');
         header.classList.remove('navbar-fixed');
     };
 };
@@ -18,3 +23,33 @@ hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+// klik di luar hamburger
+window.addEventListener('click', function(e) {
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+// Dark mode toggle
+
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+// pindah toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
